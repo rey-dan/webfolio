109384e60abc6fc7d683d08864024b8d5b2bb6cb
reyesdan30 <102945019+rey-dan@users.noreply.github.com>
2023-01-03 11:42:54 +0800

    Code update for html, css and js script

--- script.js
+++ script.js
@@ -7,88 +7,87 @@ window.addEventListener('load', function(){
     var pages = this.document.getElementsByClassName("page");
     const content = this.document.getElementById("content");
 
-    menubar.addEventListener('click', function(){
+    // For scrolling
+    var height = pages[0].scrollHeight;
+    var scrolled = content.scrollTop;
+
+    // Reusable Functions
+    var show_nav_bar = function(){
         menubar.style.display = "none";
         closebar.style.display = "inline-block";
         for(let i = 0; i < items.length; i++){
             items[i].style.display = "block";
         }
-    });
-    closebar.addEventListener('click', function(){
+    }
+
+    var close_nav_bar = function(){
         menubar.style.display = "inline-block";
         closebar.style.display = "none";
         for(let i = 0; i < items.length; i++){
             items[i].style.display = "none";
         }
-    });
+    }
+
+    var set_title = function(){
+        if(pages[i].firstElementChild.innerHTML == ""){
+            document.getElementById("title_bar").innerHTML = "DAN";
+        } else{
+            document.getElementById("title_bar").innerHTML = pages[i].firstElementChild.innerHTML;
+        }
+    }
+
+    var set_title_mobile = function(){
+        close_nav_bar();
+        if(pages[i].firstElementChild.innerHTML == ""){
+            document.getElementById("title_bar").innerHTML = "DAN";
+        } else{
+            document.getElementById("title_bar").innerHTML = pages[i].firstElementChild.innerHTML;
+        }
+    }
+
+    // Navigation bar function
+    menubar.addEventListener('click', show_nav_bar);
+    closebar.addEventListener('click', close_nav_bar);
+
+    if(this.innerWidth <= 480){
+        for(let i = 0; i < items.length; i++){
+            items[i].addEventListener('click', set_title_mobile);
+        }
+    }
 
     // Resizing adjustments for navigation bar
     this.addEventListener('resize', function(){
         if(this.innerWidth > 480){
             for(let i = 0; i < items.length; i++){
-                items[i].addEventListener('click', function(){
-                    if(pages[i].firstElementChild.innerHTML == ""){
-                        document.getElementById("title_bar").innerHTML = "DAN";
-                    } else{
-                        document.getElementById("title_bar").innerHTML = pages[i].firstElementChild.innerHTML;
-                    }
-                });
+                items[i].addEventListener('click', set_title);
             }
             menubar.style.display = "none";
             closebar.style.display = "none";
             for(let i = 0; i < items.length; i++){
                 items[i].style.display = "block";
             }
-        } else if(this.innerWidth < 480){
-            menubar.style.display = "inline-block";
-            closebar.style.display = "none";
+        }
+        
+        if(this.innerWidth <= 480){
+            close_nav_bar();
             for(let i = 0; i < items.length; i++){
-                items[i].style.display = "none";
-            }
-            
-            for(let i = 0; i < items.length; i++){
-                items[i].addEventListener('click', function(){
-                    if(pages[i].firstElementChild.innerHTML == ""){
-                        document.getElementById("title_bar").innerHTML = "DAN";
-                    } else{
-                        document.getElementById("title_bar").innerHTML = pages[i].firstElementChild.innerHTML;
-                    }
-                });
-            }
-            menubar.style.display = "inline-block";
-            closebar.style.display = "none";
-            for(let i = 0; i < items.length; i++){
-                items[i].style.display = "none";
+                items[i].addEventListener('click', set_title_mobile);
             }
         }
 
         if(this.innerWidth >= 1024){
             content.addEventListener("scroll", function(){
-                var height = pages[0].scrollHeight;
-                var scrolled = content.scrollTop;
                 for(let i = 0; i < pages.length; i++){
                     if(pages[i].offsetTop < scrolled + height/2 && scrolled < pages[i].offsetTop + height/2){
-                        if(pages[i].firstElementChild.innerHTML == ""){
-                            document.getElementById("title_bar").innerHTML = "DAN";
-                        } else{
-                            document.getElementById("title_bar").innerHTML = pages[i].firstElementChild.innerHTML;
-                        }
-                        console.log("height: " + height + "; scrolled: " + scrolled + "; offset: " + pages[i].offsetTop)        
+                        set_title();    
                     }
                 }
             });
         } else{
             this.addEventListener("scroll", function(){
-                var height = this.innerHeight;
-                var scrolled = this.scrollY;
                 for(let i = 0; i < pages.length; i++){
                     if(pages[i].offsetTop < scrolled + height/2 && scrolled < pages[i].offsetTop + height/2){
-                        if(pages[i].firstElementChild.innerHTML == ""){
-                            document.getElementById("title_bar").innerHTML = "DAN";
-                        } else{
-                            document.getElementById("title_bar").innerHTML = pages[i].firstElementChild.innerHTML;
-                        }  
-                        console.log("height: " + height + "; scrolled: " + scrolled + "; offset: " + pages[i].offsetTop)   
+                        set_title();
                     }
                 }
             });
@@ -102,12 +101,7 @@ window.addEventListener('load', function(){
             var scrolled = content.scrollTop;
             for(let i = 0; i < pages.length; i++){
                 if(pages[i].offsetTop < scrolled + height/2 && scrolled < pages[i].offsetTop + height/2){
-                    if(pages[i].firstElementChild.innerHTML == ""){
-                        document.getElementById("title_bar").innerHTML = "DAN";
-                    } else{
-                        document.getElementById("title_bar").innerHTML = pages[i].firstElementChild.innerHTML;
-                    }
-                    console.log("height: " + height + "; scrolled: " + scrolled + "; offset: " + pages[i].offsetTop)        
+                    set_title();
                 }
             }
         });
@@ -121,8 +115,7 @@ window.addEventListener('load', function(){
                         document.getElementById("title_bar").innerHTML = "DAN";
                     } else{
                         document.getElementById("title_bar").innerHTML = pages[i].firstElementChild.innerHTML;
-                    }  
-                    console.log("height: " + height + "; scrolled: " + scrolled + "; offset: " + pages[i].offsetTop)   
+                    }   
                 }
             }
         });
